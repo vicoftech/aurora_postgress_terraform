ab21375719f791088f269bf9933e2048b84e89b9
achacon3 <achacon@asap-consulting.com.ar>
2026-04-21 23:16:25 -0300

    fix otro import

--- migracion/migration_flow_partial.py
+++ migracion/migration_flow_partial.py
@@ -14,7 +14,8 @@ from sqlalchemy import create_engine, text
 
 from migration_config import MigrationConfig, MigrationResult, MigrationStatus, FlowSummary, TABLE_MIGRATION_ORDER
 from migration_tasks import migrate_table, validate_referential_integrity_task, validate_connections_task
-from migration_utils import get_run_logger, log_migration_error
+from prefect.logging import get_run_logger
+from migration_utils import log_migration_error
 
 
 @task
